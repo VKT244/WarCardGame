edfeace6bb6864e0e9a9bb86d1f2ea0ee3c16b8a
Christopher Beech <beechchristopher350@gmail.com>
2021-10-02 23:39:35 -0700

    my war card game

---
+++ testScript/test.js
@@ -0,0 +1,11 @@
+var expect = chai.expect;
+
+describe('MyFunction', function(){
+    describe('#freshDeck', function(){
+        it('should return new Card', function() {
+            new Card(suit, value);
+            freshDeck();
+            
+        })
+    })
+})
